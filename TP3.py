40304599da9f6023f9e3ee122d64eb41a7ce4c65
Val <valiktroi16@gmail.com>
2019-04-11 19:37:15 -0300

    Estoy por el punto 4

--- TP3.py
+++ TP3.py
@@ -1,6 +1,6 @@
 #1)
 
-palabra = input("Ingrese una palabra: ")
+palabra = str(input("Ingrese una palabra: "))
 for i in range(1,1001):
     print(palabra, end=" ")
 print()
@@ -14,6 +14,28 @@ for i in range(6):
         print(x," | ",i + 1)
 
 #3)
+
+n = int(input("Ingrese un numero: "))
+suma = 0
+for i in range(1, n + 1):
+    cont = i
+    suma = suma + cont
+print(suma)
+
+#4)
+
+c1 = []
+c2 = []
+c3 = []
+for i in range(2):
+    a = int(input("Ingrese una coordenada x y luego una y: "))
+    c1.append(a)
+    b = int(input("Ingrese una coordenada x y luego una y: "))
+    c2.append(b)
+    c = int(input("Ingrese una coordenada x y luego una y: "))
+    c3.append(c)
+
+
 
 
     
