21988f46019ae3c579b052ed55eaff77681806dc
Val <valiktroi16@gmail.com>
2019-04-11 18:10:30 -0300

    Tp3 negro de mierda

---
+++ TP3.py
@@ -0,0 +1,23 @@
+#1)
+
+palabra = input("Ingrese una palabra: ")
+for i in range(1,1001):
+    print(palabra, end=" ")
+print()
+print()
+
+#2)
+
+print("0  |  0")
+for i in range(6):
+    for x in range(7):
+        print(x," | ",i + 1)
+
+#3)
+
+
+    
+
+                                                                                                                                                                                                                                        
+                                                                                                                                                                                                                                                
+
